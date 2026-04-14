0dd4b35a7f2d693dfd2654125372db44361bca72
Afroj <overthinker.ltd03@gmail.com>
2026-04-14 15:56:05 +0530

    Add variables and comments examples

---
+++ variables.py
@@ -0,0 +1,20 @@
+ #let's start with how to do comment in python then we will move to variables in this file.
+ # this hashtag is used for single line comment in python.
+
+"""
+Hii, this is a multi line
+comment you can write as much you need under it.
+comment used for explaination, todo future work and disable the code.
+"""
+
+#Now let's move to var 
+"""
+so Variable a memory unit like a container that hold data value.
+variable_name = value
+int is -1 , 2 (integers num)
+float is real num  9.99 (decimal num)
+use underscore _ to separate words in var name.
+"""
+price = 9.99
+apple = 2
+player_cassie = 0
