1c3a27b6ac278e91b4bbadbd442212855050b37c
Afroj <overthinker.ltd03@gmail.com>
2026-04-19 22:26:37 +0530

    do at comment about how it works

--- operators.py
+++ operators.py
@@ -13,3 +13,5 @@ print(a / b)   # 3.3333 → division (always float)
 print(a // b)  # 3   → floor division (removes decimal)
 print(a % b)   # 1   → modulus (remainder)
 print(a ** b)  # 1000 → power (10³)
+
+#after run you can see this on output panel or type python + file name in your terminal and you will see the number output. 
