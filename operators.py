5cf855fca53e3b139255ad7672f7bc3130b101b5
Afroj <overthinker.ltd03@gmail.com>
2026-04-20 08:28:41 +0530

    Added comparison operatoers

--- operators.py
+++ operators.py
@@ -15,3 +15,17 @@ print(a % b)   # 1   → modulus (remainder)
 print(a ** b)  # 1000 → power (10³)
 
 #after run you can see this on output panel or type python + file name in your terminal and you will see the number output. 
+
+# ── 2. COMPARISON OPERATORS ───────────────
+# These compare two values
+# Result is always True or False (Boolean!)
+
+a = 10
+b = 3
+
+print(a == b)   # False → equal to
+print(a != b)   # True  → not equal to
+print(a > b)    # True  → greater than
+print(a < b)    # False → less than
+print(a >= b)   # True  → greater or equal
+print(a <= b)   # False → less or equal
