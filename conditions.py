6681a36c240e3553e34105d62837bd10e1311225
Afroj <overthinker.ltd03@gmail.com>
2026-04-22 22:15:09 +0530

    Add conditional statements

---
+++ conditions.py
@@ -0,0 +1,11 @@
+# ── IF / ELSE CONDITIONS ───────────────────
+# Used to make decisions in your program
+# If this is true → do this
+# Otherwise → do that
+
+age = 20
+
+if age >= 18:
+    print("You are an adult!")
+else:
+    print("You are a minor!")
