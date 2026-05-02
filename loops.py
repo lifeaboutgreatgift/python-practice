6aa6dc3d7f42e06fc9a1cf7e25d5613163b4621d
Afroj <overthinker.ltd03@gmail.com>
2026-05-02 23:21:25 +0530

    Add new topic~ Loops in python

---
+++ loops.py
@@ -0,0 +1,16 @@
+# ── LOOPS ─────────────────────────────────
+# Loops repeat code multiple times
+# Without loops → you'd write same code 100x
+# With loops → write once, run 100x! 🔥
+
+# ── 1. FOR LOOP ───────────────────────────
+# Used when you know HOW MANY times to repeat
+# Used in: lists, data processing, games
+
+# basic for loop
+for i in range(5):
+    print(i)          # prints 0,1,2,3,4
+
+# range with start and end
+for i in range(1, 6):
+    print(i)          # prints 1,2,3,4,5
