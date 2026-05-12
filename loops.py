710b45a60c3415eb4a32cc0cfce95ce814fde4c8
Afroj <overthinker.ltd03@gmail.com>
2026-05-13 00:25:51 +0530

    feat: added example

--- loops.py
+++ loops.py
@@ -248,3 +248,19 @@ while True:
     else:
         print(f"Correct! You got it in {attempts} attempts!")
         break
+
+rows = 6
+for i in range(rows):
+    for j in range(i):
+        print(i, end=' ')
+    print('')
+
+rows = 5
+k = 2 * rows - 2
+for i in range(rows, -1, -1):
+    for j in range(k, 0, -1):
+        print(end=" ")
+    k = k + 1
+    for j in range(0, i + 1):
+        print("*", end=" ")
+    print("")
